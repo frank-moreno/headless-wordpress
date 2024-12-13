7836e8ab4cf5a1648bc3bfc337f47aa2a96667b3
frank-moreno <hello@frank-moreno.com>
2024-12-13 05:27:09 +0000

    default posts restapi on progress

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,13 @@
 import React from 'react';
+import PostList from "../components/Posts/PostList";
+import "./../styles/main.scss";
 
 const Home = () => {
   return (
-    <main>
-      <section className="main-map">
-        <div className="container">
-        <h1>Headless Wordpress Project</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit metus at aliquet lacinia. Vivamus eget ante a velit volutpat mollis sit amet a orci.
-        </p>
-      </div>
-      </section>
-    </main>
+    <div className="homepage container">
+      <h1>Welcome to My Blog</h1>
+      <PostList />
+    </div>
   );
 };
 
